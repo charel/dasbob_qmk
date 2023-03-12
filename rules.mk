0ddcd3866c950b6d610973831dd9cff783d87547
kilipan <kilipan@tutanota.com>
2023-03-12 15:27:13 +0100

    initial commit

---
+++ rules.mk
@@ -0,0 +1,33 @@
+# MCU name
+MCU = RP2040
+
+# Bootloader selection
+BOOTLOADER = rp2040
+
+ALLOW_WARNINGS = yes
+
+# LTO must be disabled for RP2040 builds
+LTO_ENABLE = no
+
+# PIO serial/WS2812 drivers must be used on RP2040
+SERIAL_DRIVER = vendor
+WS2812_DRIVER = vendor
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+UNICODE_ENABLE = yes        # Unicode
+AUDIO_ENABLE = no           # Audio output
+SPLIT_KEYBOARD = yes        # Use shared split_common code
+SWAP_HANDS_ENABLE = yes
+# OLED_ENABLE = yes
+# OLED_DRIVER = SSD1306
+# WPM_ENABLE = yes
